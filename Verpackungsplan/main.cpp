72c5743d6469199146197f0ad590598da489fa05
Anrei Lukashevich <And_Box@iCloud.com>
2025-04-05 09:35:54 +0200

    (5|9)\d{5}[^(\d|[:alpha:]|)]("|[[:s:]])*

--- Verpackungsplan/main.cpp
+++ Verpackungsplan/main.cpp
@@ -6,8 +6,12 @@
 //  g++ main.cpp -std=c++23 -Wall -o main
 //  ./main /Users/and/Downloads/Makefile.txt DEBUG
 //  rm ./main; clear; g++ main.cpp -std=c++23 -Wall -o main; ls -alSF
-//  clear ; ~/workspace/Verpackungsplan/Verpackungsplan/main /Users/and/Downloads/return/test.txt "(\\w{7,})"
-//  clear ; ~/workspace/Verpackungsplan/Verpackungsplan/main /Users/and/Downloads/return/test.txt "\d{6}[9]"
+//  clear; ~/workspace/Verpackungsplan/Verpackungsplan/main ~/Downloads/return/test.txt "\\d{6}[^(\\d|:alpha:)]"
+//  clear; ~/workspace/Verpackungsplan/Verpackungsplan/main ~/Downloads/Verpackungsplan/data_test_1.txt "[\\d(9|5)]\\d{5}"
+//  clear; ~/workspace/Verpackungsplan/Verpackungsplan/main ~/Downloads/Verpackungsplan/data_test_1.txt "[\\d(9|5)]\\d{5}"
+
+//rm ~/workspace/Verpackungsplan/Verpackungsplan/main; clear; g++ ~/workspace/Verpackungsplan/Verpackungsplan/main.cpp -std=c++23 -Wall -o ~/workspace/Verpackungsplan/Verpackungsplan/main
+
 
 #include <iostream>
  
@@ -17,16 +21,16 @@
 #include <string>
 #include <filesystem>
 #include <cstring>
+//#include <algorithm>
+
 
 
 using namespace std;
 using namespace filesystem;
 
-static vector<pair<size_t, string>> matches (const path &, const char*, const char* ); //const regex &, const regex &
+static vector<pair<string, string>> matches (const path &, const char*);
 
 int main(int argc, const char * argv[]) {
-    
-    
     if (argc<=2) {
         std::cout<< "Usage:"<<argv[0]<<"<path>\n";
         std::printf("Usage: %s <path>\n", argv[0]);
@@ -39,35 +43,48 @@ int main(int argc, const char * argv[]) {
     }
     std::printf("\n %s The Path exist\n\n",canonical(dir).c_str());
     std::printf(" [0] %s\n [1] %s\n [2] %s\n\n",argv[0],argv[1],argv[2]);
-//    regex pattern;
-//    try {
-//        pattern =regex{argv[1]};
-//    } catch (const regex_error &err) {
-//        std::printf("Invalid regular expression provided.n");
-//        return 1;
-//    }
-    
-    vector<pair<size_t, string>> data ( matches(argv[1], argv[2], argv[2]));
-    for (const auto &[number, content]: data) {
-        printf(" [%5i]  -->  %s\n", (int)number, content.c_str());
+
+    try {
+        auto data ( matches(argv[1], argv[2]));
+        for (const auto &[result, content]: data) {
+            printf(" [%s]  -->  %s\n", result.c_str(), content.c_str());
+        }
+    } catch (const regex_error &err) {
+        std::printf("Invalid regular expression provided.\n");
+        return 1;
     }
-    
-    
+  
     std::printf("\n OK!\n") ;
     return 0;
 }
 
-static vector<pair<size_t, string>> matches (const path & path_to, const char*  reg_of_fir = "", const char* reg_of_sec = ""){
-    vector<pair<size_t, string>> data_vec;
-    ifstream ifstrm{path_to.c_str()};  // "/Users/and/Downloads/Makefile.txt"
-    string s;
-    for (size_t line{1}; getline(ifstrm, s); ++line) {
-        //printf("%s", s.c_str());
-
-        if (regex_search(begin(s), end(s),regex{reg_of_fir,std::regex_constants::ECMAScript | std::regex_constants::icase})) {
-            data_vec.emplace_back(line,std::move(s));
+static vector<pair<string, string>> matches (const path & path_to, const char*  reg_of_fir = ""){
+    vector<pair<string, string>> data_vec;
+    ifstream ifstrm{path_to.c_str()};
+    string str_all,str_mach;
+    smatch result;
+    
+    
+    for (;getline(ifstrm, str_all);) {
+        regex reg{reg_of_fir,std::regex_constants::ECMAScript | std::regex_constants::icase};
+        if (regex_search(str_all,result,reg)) {   //regex_search(begin(s), end(s),reg)
+            str_mach = result.str();
+            str_mach.erase(6, str_mach.length());  // shrink to 6 digits
+            data_vec.emplace_back(str_mach, result.suffix());
         }
     }
+    
+    
+    
     return data_vec;
 }
 	
+pair<string, string> pattern_and_substring (const string & source, const string & pattern){
+    smatch result;
+
+    regex reg{pattern,std::regex_constants::ECMAScript | std::regex_constants::icase};
+    if (regex_search(source,result,reg)) {   //regex_search(begin(s), end(s),reg)
+
+    }
+    return pair<string, string>(result.str(),result.suffix());
+}
