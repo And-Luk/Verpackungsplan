1c9361d95c1f4e2a4a288765d7606b7b3939f790
Anrei Lukashevich <And_Box@iCloud.com>
2025-04-05 17:03:24 +0200

    "(5|9)\\d{5}[^(\\d|[:alpha:]|)](\"|[[:space:]])*"

--- Verpackungsplan/main.cpp
+++ Verpackungsplan/main.cpp
@@ -28,7 +28,8 @@
 using namespace std;
 using namespace filesystem;
 
-static vector<pair<string, string>> matches (const path &, const char*);
+static vector<tuple<string, string, string>> matches (const path &, const char*);
+pair<string, string> substring (const string & source, const string & pattern);
 
 int main(int argc, const char * argv[]) {
     if (argc<=2) {
@@ -45,9 +46,9 @@ int main(int argc, const char * argv[]) {
     std::printf(" [0] %s\n [1] %s\n [2] %s\n\n",argv[0],argv[1],argv[2]);
 
     try {
-        auto data ( matches(argv[1], argv[2]));
-        for (const auto &[result, content]: data) {
-            printf(" [%s]  -->  %s\n", result.c_str(), content.c_str());
+        auto data (matches(argv[1], argv[2]));
+        for (const auto &[ID,result,content]: data) {
+            printf(" [%s]  [%s] -->  %s\n",ID.c_str(), result.c_str(), content.c_str());
         }
     } catch (const regex_error &err) {
         std::printf("Invalid regular expression provided.\n");
@@ -58,19 +59,37 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-static vector<pair<string, string>> matches (const path & path_to, const char*  reg_of_fir = ""){
-    vector<pair<string, string>> data_vec;
+static vector<tuple<string,string,string>> matches (const path & path_to, const char*  reg_of_fir = ""){
+    vector<tuple<string,string, string>> data_vec;
     ifstream ifstrm{path_to.c_str()};
-    string str_all,str_mach;
+    string str_all,str_temp, str_old{};
+    size_t ident=0;
     smatch result;
-    
+    pair<string, string> pair_temp;
     
     for (;getline(ifstrm, str_all);) {
         regex reg{reg_of_fir,std::regex_constants::ECMAScript | std::regex_constants::icase};
         if (regex_search(str_all,result,reg)) {   //regex_search(begin(s), end(s),reg)
-            str_mach = result.str();
-            str_mach.erase(6, str_mach.length());  // shrink to 6 digits
-            data_vec.emplace_back(str_mach, result.suffix());
+            str_temp = result.str();
+            str_temp.erase(6, str_temp.length());  // shrink to 6 digits
+            
+            if (str_temp[0]=='9' ) { //&& ident ==0
+                ident = 1;
+                pair_temp = substring(result.suffix(), string{"(9|5)\\d{5}[^(\\d|[:alpha:]\\|)](\"\\|\\\\:|[[:space:]])*"});
+                data_vec.emplace_back(str_temp,pair_temp.first, pair_temp.second);
+                str_old=str_temp;
+                continue;
+            }
+            if (ident==1) {
+                
+                data_vec.emplace_back(str_old, str_temp, result.suffix());
+                //ident = 0;
+                continue;
+            }
+            
+            data_vec.emplace_back(str_old, str_temp, result.suffix());
+            //str_old=str_temp;
+            ident = 0;
         }
     }
     
@@ -79,12 +98,18 @@ static vector<pair<string, string>> matches (const path & path_to, const char*  
     return data_vec;
 }
 	
-pair<string, string> pattern_and_substring (const string & source, const string & pattern){
+pair<string, string> substring (const string & source, const string & pattern){
     smatch result;
-
+    string str_temp;
+    
     regex reg{pattern,std::regex_constants::ECMAScript | std::regex_constants::icase};
     if (regex_search(source,result,reg)) {   //regex_search(begin(s), end(s),reg)
-
+        str_temp = result.str();
+        str_temp.erase(6, str_temp.length());  // shrink to 6 digits
+        
+        
     }
-    return pair<string, string>(result.str(),result.suffix());
+    return pair<string, string>(str_temp,result.suffix());
 }
+
+//tuple<string,string,string>
