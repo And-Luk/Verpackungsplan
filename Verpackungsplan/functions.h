ef7d45ddf250cec2a30ee2b2b13f04d8e61e69bc
Anrei Lukashevich <And_Box@iCloud.com>
2025-04-13 15:50:47 +0200

    search 955342 524859    UFO Bio-Bratwurstsortiment

--- Verpackungsplan/functions.h
+++ Verpackungsplan/functions.h
@@ -8,7 +8,6 @@
 #include "header.h"
 
 
-
 vector<tuple<string, string, string>> matches (const path &, const char*);
 
 pair<string, string> substring (const string & , const char*);
@@ -20,10 +19,7 @@ deque<size_t> read_verpack_txt(const path &, const char*);
 void write_RTF(const path &, const ostringstream &);
 
 typedef pair<const size_t, string> subI_subS;
-//typedef multimap<size_t, subI_subS, std::less<size_t>, std::allocator<std::pair<const size_t, subI_subS>>> multimap_data;
-typedef multimap<size_t, subI_subS> multimap_data;
-
-
+typedef multimap<size_t, subI_subS, std::less<size_t>, std::allocator<std::pair<const size_t, subI_subS>>> multimap_data;
+//typedef multimap<size_t, subI_subS> multimap_data;
 auto read_data_txt(const path & , const char*)-> multimap_data;
-
 
--- Verpackungsplan/main.cpp
+++ Verpackungsplan/main.cpp
@@ -5,7 +5,6 @@
 //  Created by Andrei Lukashevich on 2025-03-25.
 
 
-
 #include "functions.h"
 
 
@@ -23,26 +22,26 @@ int main(int argc, const char * argv[]) {
     std::printf("\n %s The Path exist\n\n",canonical(dir).c_str());
     std::printf(" [0] %s\n [1] %s\n [2] %s\n\n",argv[0],argv[1],argv[2]);
 //
-    try {
-        auto data (matches(argv[1], argv[2]));
-        for (const auto &[ID_Operation,result,content]: data) {
-            printf(" [%s]  [%s] -->  %s\n",ID_Operation.c_str(), result.c_str(), content.c_str());
-        }
-    } catch (const regex_error &err) {
-        std::printf("Invalid regular expression provided.\n");
-        return 1;
-    }
-    try {
-        const std::filesystem::path dir{"/Users/and/Downloads/Verpackungsplan/RTF_in.rtf"};
-        if (!exists(dir)) {
-            std::printf("Can'n open the RTF file.\n\n");
-            return 1;
-        }
-        
-        read_write_RTF ("/Users/and/Downloads/Verpackungsplan/RTF_in.rtf", "/Users/and/Downloads/Verpackungsplan/RTF_out.rtf");
-    } catch (std::exception ex){
-        std::printf(" ERROR of writing the out file \n");
-    }
+//    try {
+//        auto data (matches(argv[1], argv[2]));
+//        for (const auto &[ID_Operation,result,content]: data) {
+//            printf(" [%s]  [%s] -->  %s\n",ID_Operation.c_str(), result.c_str(), content.c_str());
+//        }
+//    } catch (const regex_error &err) {
+//        std::printf("Invalid regular expression provided.\n");
+//        return 1;
+//    }
+//    try {
+//        const std::filesystem::path dir{"/Users/and/Downloads/Verpackungsplan/RTF_in.rtf"};
+//        if (!exists(dir)) {
+//            std::printf("Can'n open the RTF file.\n\n");
+//            return 1;
+//        }
+//        
+//        read_write_RTF ("/Users/and/Downloads/Verpackungsplan/RTF_in.rtf", "/Users/and/Downloads/Verpackungsplan/RTF_out.rtf");
+//    } catch (std::exception ex){
+//        std::printf(" ERROR of writing the out file \n");
+//    }
     
 //    try {
 //        const std::filesystem::path dir{"/Users/and/Downloads/Verpackungsplan/verpack.txt"};
@@ -68,17 +67,19 @@ int main(int argc, const char * argv[]) {
         }
 
         auto list_of_ID = read_data_txt("/Users/and/Downloads/Verpackungsplan/data_test_1.txt","(5|9)\\d{5}[^(\\d|[:alpha:]|)](\"|[[:space:]])*");
-        for (auto & artikel: list_of_ID) {
-            std::printf("   %i  ->  %i  ->  %s\n", (int)artikel.first, (int)artikel.second.first, artikel.second.second.c_str());
-            }
         
-//        for (auto element= list_of_ID.begin(); element==list_of_ID.end(); ++element) {
-//            std::printf("   %i  ->  %i  ->  %s\n", (int)element->first, (int)element->second.first, element->second.second.c_str());
+//        for (auto & artikel: list_of_ID) {
+//            std::printf("   %i    %i    %s\n", (int)artikel.first, (int)artikel.second.first, artikel.second.second.c_str());
 //            }
+//        std::printf("and   \n\n\n");
         
         
-        
-        
+        for (auto it =  list_of_ID.upper_bound((size_t)955342) ; it == list_of_ID.lower_bound((size_t)955342); ++it) {
+            std::printf("element   %i  ->  %i  ->  %s\n", (int)it->first, (int)it->second.first, it->second.second.c_str());
+            }
+        cout<<"955342"<<" "<< list_of_ID.find((size_t)955342)->second.first<< "    "<< list_of_ID.find((size_t)955342)->second.second;
+        //cout<< list_of_ID.find((size_t)955342)->second.second;
+
     } catch (std::exception ex) {
         std::printf(" ERROR of reading the verpack.txt file to DATA MULTIMAP \n\n");
         std::printf("\n An exception!\n") ;
