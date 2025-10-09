b8dee7fd1817ce9caf01d749d25d272aadbf6912
Andrei Lukashevich <And_Box@iCloud.com>
2025-10-09 17:02:26 +0200

    photo.cpp

---
+++ Verpackungsplan/photo.h
@@ -0,0 +1,19 @@
+//
+//  photo.h
+//  Verpackungsplan
+//
+//  Created by Andrei Lukashevich on 08-10-2025.
+//
+#pragma once
+
+#include "header.h"
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
+
+
+
+int take_a_photo();
---
+++ Verpackungsplan/read_Excel_file.h
@@ -0,0 +1,15 @@
+//
+//  read_Excel_file.h
+//  Verpackungsplan
+//
+//  Created by Andrei Lukashevich on 28-09-2025.
+//
+#pragma once
+#include "header.h"
+#include <iostream>
+#include "libxl/include_cpp/libxl.h"
+
+using namespace libxl;
+
+
+int read_Excel(const char* input);
